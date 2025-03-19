a5bb211650c558ed0591cc504ae7bb8a3c4a591a
ocheezyyy <seanodonnellse@gmail.com>
2025-03-19 00:36:35 -0400

    Updated types and removed files/methods that are no longer needed as they moved to the express api.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,7 @@
 export type Difficulty = "easy" | "medium" | "hard" | "mixed";
 export type TimeLimit = "15" | "30" | "45" | "60";
+export type SocketSendEvents = "createRoom" | "joinRoom" | "submitAnswer" | "sendMessage";
+export type SocketResponseEvents = "roomCreated" | "playerJoined" | "updatePlayerScore" | "joinFailed" | "receivedMessage";
 
 export type Question = {
     type: "multiple" | "boolean";
@@ -11,17 +13,24 @@ export type Question = {
 }
 
 export type Player = {
+    id: string;
     name: string;
     score: number;
 }
 
-export type TriviaResponse = {
-    response_code: number;
-    results: Question[];
-};
-
 export type Message = {
     message: string;
     user: string;
-    date: Date;
 }
+
+export type RoomData = {
+    gameId: string;
+    players: Player[];
+    host: string;
+    questions: Question[];
+    messages: Message[];
+    currentQuestion: number;
+    gameStarted: boolean;
+    category: number;
+    difficulty: Difficulty;
+};
